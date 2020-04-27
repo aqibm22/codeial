1bd36c3d510bd7b6aa52acb22a546a01a0b1e29b
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-04-27 13:52:42 +0530

    now post form is only visible to users who are signed in, 2 step check- 1st at home.ejs and 2nd at posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create' , postsController.create);
+//double check as to only signed in user can create a psot
+router.post('/create' , passport.checkAuthentication, postsController.create);
 
 module.exports = router;
