5f4bbef16f0bc2724a7ce5355626aadd5213e70a
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-04-27 13:38:20 +0530

    Posts displayed in home.ejs along with author for which we used populate function, see home_controller.js

--- controllers/home_contoller.js
+++ controllers/home_contoller.js
@@ -1,3 +1,6 @@
+const Post = require('../models/post');
+
+
 // all functiomn here is accesed by controller
 ///format- 
 // module.exports.actionName = function(req,res){
@@ -5,10 +8,23 @@
 //} // see routers as to how and when to use functions
 module.exports.home = function(req,res){
     //to see cookies--
-    console.log(req.cookies);
+    //console.log(req.cookies);
     //to change a cookie --
     // res.cookie('yoo',90); //yoo is id , 90 is value
-    return res.render('home',{
-        title: "Home"
+    // to display all the posts -
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title: "Codeilal | Home",
+    //         posts: posts 
+    //     });
+    // }); 
+
+    // populate the user of this post, whole user will be sent along with the post, we just display the name there in homee.ejs
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts 
+        });
     });
 }
+
