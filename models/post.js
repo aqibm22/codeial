99f8fd25dfc81b3c6fe744ddf11139c8376f8235
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-04-27 17:04:34 +0530

    comment schema is created in comment.js in models, and array called comments is added in post schema in post.js

--- models/post.js
+++ models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const postSchema = new mongoose.Schema({
     content:{
         type: String,
@@ -9,8 +8,15 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
-
+    },
+    // include the id's of all the comments in this post schema itself in an array called comments
+    // for quick fetching on comments when a  post is displayed
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 },{
     timestamps: true
 });
