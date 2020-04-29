7b6b88a6158393aa681e13d4e2a87bc4c31914a2
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-04-29 17:12:32 +0530

    async await added in home_controller.js

--- controllers/home_contoller.js
+++ controllers/home_contoller.js
@@ -6,59 +6,31 @@ const User = require('../models/user');
 // module.exports.actionName = function(req,res){
 //    ....
 //} // see routers as to how and when to use functions
-module.exports.home = function(req,res){
-    //to see cookies--
-    //console.log(req.cookies);
-    //to change a cookie --
-    // res.cookie('yoo',90); //yoo is id , 90 is value
-    // to display all the posts -
-    // Post.find({}, function(err, posts){
-    //     return res.render('home',{
-    //         title: "Codeilal | Home",
-    //         posts: posts 
-    //     });
-    // }); 
 
-    // populate the user of this post, whole user will be sent along with the post, we just display the name there in homee.ejs
-    // Post.find({}).populate('user').exec(function(err,posts){
-    //     return res.render('home',{
-    //         title: "Codeial | Home",
-    //         posts: posts 
-    //     });
-    // });
+// this is after  async await is done , if you want to see the prevoius versions see the comment on or  before 28th April, 2020
 
-    // Post.find({})
-    // .populate('user') // user sent along with the post as done above
-    // .populate({
-    //     path: 'comments',
-    //     populate: {
-    //         path: 'user' // the author of the comment is sent for every post
-    //     }
-    // })
-    // .exec(function(err,posts){
-    //     return res.render('home',{
-    //         title: "Codeial | Home",
-    //         posts: posts 
-    //     });
-    // });
+module.exports.home = async function(req,res){
+    
+    try{
+        let posts = await Post.find({})
+        .populate('user') // user sent along with the post as done above
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user' // the author of the comment is sent for every post
+            }
+        })
+        let users =  await User.find({});
 
-    Post.find({})
-    .populate('user') // user sent along with the post as done above
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user' // the author of the comment is sent for every post
-        }
-    })
-    .exec(function(err,posts){
-        // using user to send all the users because we need to display it
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
-            });
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
         });
-    });
+        
+    }catch(err){
+        console.log('Error' , err);
+        return;
+    } 
 }
 
