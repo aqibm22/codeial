8a576f3dedb5ecf665144249a798c185195d5b81
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-04-29 17:42:32 +0530

    Async await added to destroy,create function in comments_controller.js

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,41 +1,48 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    //check if the post on which the comment, that post id exists in db Post
-    Post.findById(req.body.post , function(err,post){ // post variable will hold the post whose id was queried by req.body.post
+
+// async await added in both create and destroy function 
+// to see explanation of code see commits before 29th April, 2020
+module.exports.create = async function(req, res){
+   
+    try{
+        let post = await Post.findById(req.body.post);
+    
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content : req.body.content,
-                post : req.body.post, // 1) place the post id in the comments as field post
+                post : req.body.post,
                 user: req.user._id
-            }, function(err,comment){
-                if(err){
-                    console.log('Error in creating comment');
-                    return;
-                }
-                post.comments.push(comment); // 2) push the created comment into the comments array of database Post
-                post.save(); // as we updated in database Post  we use post.save();
-                res.redirect('/');
             });
+
+            post.comments.push(comment); 
+            post.save(); 
+            res.redirect('/');
         }
-    });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
 
 //same as delete post.
-module.exports.destroy = function(req,res){
-    Comment.findById(req.params.id, function(err,comment){
-        if(comment.user == req.user.id){ //.id here converts it to string
+module.exports.destroy = async function(req,res){
+    
+    try{
+        let comment = await Comment.findById(req.params.id);
+        if(comment.user == req.user.id){ 
 
-            let postId = comment.post; // this is the post where we have to delete the current comment
-            // removing the desired comment from db Comment
+            let postId = comment.post; 
             comment.remove(); 
-            // pulling out desired comment which is in req.params.id from postId/comments array by using the below function - 
-            Post.findByIdAndUpdate(postId , { $pull: {comments: req.params.id} }, function(err, post){
-                return res.redirect('back');
-            });
+            let post = Post.findByIdAndUpdate(postId , { $pull: {comments: req.params.id}});
+            return res.redirect('back');
         }else{
             return res.redirect('back');
         }
-    });
-}
+    }
+    catch(err){
+        console.log('Error',err);
+        return;
+    }
+};
