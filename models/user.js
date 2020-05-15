06da29ceaa14c7f5cf2348f835e06336366c6c34
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-05-15 11:42:02 +0530

    Setting up storage for files using mutter in user.js

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const multer = require('multer');
 const path = require('path');
+// the path of the avatar folder 
 const AVATAR_PATH = path.join('/uploads/users/avatar'); 
 
 const userSchema = new mongoose.Schema({
@@ -17,11 +18,24 @@ const userSchema = new mongoose.Schema({
     name : {
         type : String,
         required : true
+    },
+    avatar : {
+        type : String
     }
 }, {
     // when it is created and updated
     timestamps : true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..' , AVATAR_PATH));
+      // this creates the path of parent of user.js + AVATAR_PATH we have above
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+})
 
 const user = mongoose.model('User' , userSchema);
 
