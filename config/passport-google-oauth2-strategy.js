226b3e2ce3719c3a9fa3735b327d41fbce158d54
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-05-19 17:47:38 +0530

    Social Authentication done. Now sign-up/sign-in can be done with the help of google account

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID: '528894417815-59gckq2k1u9tt2tlr104btgjuouhao2t.apps.googleusercontent.com', 
+        clientSecret: 'W8UKVfkVRtpocz86rkr_S9lo', 
+        callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+
+    function(accessToken, refreshToken, profile, done){
+        // find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if (err){console.log('error in google strategy-passport', err); return;}
+            console.log(accessToken, refreshToken);
+            console.log(profile);
+
+            if (user){
+                // if found, set this user as req.user
+                return done(null, user);
+            }else{
+                // if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if (err){console.log('error in creating user google strategy-passport', err); return;}
+
+                    return done(null, user);
+                });
+            }
+
+        }); 
+    }
+
+
+));
+
+
+module.exports = passport;
