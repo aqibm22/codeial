ad6da239a1b442514cd7d508fdf280eb3b9ec685
Aqib Mahboob <aqibmahboob1999@gmail.com>
2020-05-20 20:11:21 +0530

    Template for sending email done

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,13 +2,13 @@ const nodeMailer = require('../config/nodemailer');
 
 // this is another way of exporting a function 
 exports.newComment = (comment) => {
-    console.log('inside newComment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment: comment} , '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'Codeial | AqibM',
         to: comment.user.email,
         subject: 'New Comment Published',
-        html: '<h1>Yup your comment is published</h1>'
+        html: htmlString
     }, (err , info) => {
         if(err){
             console.log('Error in sending mail' , err);
